4dd184675db38edb397e3c04cff0824d7d0e5397
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:16:01 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string login;
+string password;
+Console.WriteLine("Veuillez entrer votre identifiant");
+login = Console.ReadLine();
+Console.WriteLine("Veuillez entrer votre mot de passe");
+password = Console.ReadLine();
+switch (login, password)
+{
+    case ("Manu","lamanu"):
+        Console.WriteLine("Bienvenue à la Manu.");
+        break;
+    default:
+        Console.WriteLine("Mauvais identifiant ou mauvais mot de passe.");
+        break;
+}
